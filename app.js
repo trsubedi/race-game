aad2c9dc9be510ab0c11cee457e69e7a94c9a884
shiva subedi <shivasubedi@shiva.mac.com>
2015-08-23 18:22:49 -0700

    first commit

--- app.js
+++ app.js
@@ -1,41 +1,65 @@
 // OOP Racing Game example boilerplate code
-
+$(document).ready(function(){
 function Game() {
+  var game = new Game();
+  game.init();
+
   //Create a new instance of player 1
-  //this.player1 = ...
+  this.player1 = new Player("joe");
 
   //Do the same for a player 2
-  //this.player2 = ...
+  this.player2 = new Player("john");
 
   //Create the track
-  //this.track = ...
+  this.track = new Track();
+
+  this.init();
 }
 
 // `Game.prototype.init` kicks off a new game with a board and two players
 Game.prototype.init = function() {
-  //
-};
+  $(document).keydown(function() { 
+    // if keydown == a
+    if(event.which ===65){
+      var left = $("#player1").css("left");
+      $("#player1").css("left", (parseInt(left) + 5).toString() + "px" ) ;
+    }
+    // else if keydown == L
+    else if(event.which ===76){
+      var left = $("#player2").css("left");
+      // console.log(parseInt(left).toString());  // "10px"
+      $("#player2").css("left", (parseInt(left) + 5).toString() + "px" ) ;  // 2 + 5  px
+
+       }
+  });
+
+}
 
 // A starter Player constructor.
-function Player(team) {
-  //this.name = ...
-  //this.position = ...
+function Player(name) {
+  this.name = name;
+  this.position = 0;
 };
 
 // Remember: prototypes are shared functions between all game instances
 Player.prototype.move = function() {
-  //update player's position
-};
+  var left = $(this).css("left");
+  // console.log(parseInt(left).toString());  // "10px"
+  $(this).css("left", (parseInt(left) + 5).toString() + "px" ) ;  // 2 + 5  px
+
+
+}
+
 
 
 // A starter Track constructor.
-function Track() {
-  //Tracks the cells of the board instance
-  //this.$cells = ...
+// function Track() {
+//   this.start=
+//   this.finish=
+//   //Tracks the cells of the board instance
+//   //this.$cells = ...
 
-  //Store any other properties that board may have below, such as a reset option
-};
+//   //Store any other properties that board may have below, such as a reset option
+// }
 
-// Start the game!
-var game = new Game();
-game.init();
+});
